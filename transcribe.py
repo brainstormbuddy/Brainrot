49ad23e43dac71ac7c46c7bebb0bed6c39cf4b48
Adam Sims <adamsims1203@gmail.com>
2024-02-10 08:59:17 -0500

    transcribe python inference

--- transcribe.py
+++ transcribe.py
@@ -1,20 +1,28 @@
 from flask import Flask, jsonify, request
-import whisper
+import whisper_timestamped as whisper
 
 app = Flask(__name__)
 
 @app.route('/transcribe', methods=['POST'])
 def transcribe_audio():
-    data = request.json
-    audio_path = data.get('audio_path')  # Use the correct key to get the audio path
-    
-    # Load and transcribe the audio
-    audio = whisper.load_audio(audio_path)
-    model = whisper.load_model("tiny", device="cpu")
-    result = whisper.transcribe(model, audio, language="en")
-    
-    # Return the transcription result as JSON
-    return jsonify(result)
+    res = []
+    try:
+        data = request.json
+        audios = data.get('audios')
+        
+        if not audios:
+            raise ValueError("The 'audios' is not provided in the request.")
+        
+        for audio_path in audios:
+            # Load and transcribe the audio
+            audio = whisper.load_audio(audio_path)
+            model = whisper.load_model("tiny", device="cpu")
+            transcribed = whisper.transcribe(model, audio, language="en")
+            res.append((transcribed, audio_path))
+        return jsonify(res)
+    except Exception as e:
+        app.logger.error(f'An error occurred during transcription: {e}')
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)  # Run in debug mode
+    app.run(debug=True)

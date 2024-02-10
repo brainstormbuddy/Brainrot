8427ce715fc60793369f6b93e5b4e7226d89ffec
Adam Sims <adamsims1203@gmail.com>
2024-02-10 11:39:15 -0500

    dynamic agent inputs

--- transcript.mjs
+++ transcript.mjs
@@ -6,12 +6,21 @@ const openai = new OpenAI({
 	apiKey: process.env.OPENAI_API_KEY,
 });
 
-export default async function transcriptFunction(topic) {
+export default async function transcriptFunction(topic, agentA, agentB) {
 	const completion = await openai.chat.completions.create({
 		messages: [
 			{
 				role: 'system',
-				content: `Craft a dialogue for a short-form dialogue featuring a conversation between Joe Rogan and Jordan Peterson on the topic of ${topic}. The dialogue should be infused with humor to engage viewers while also providing insightful commentary on the subject matter. The dialogue should follow the characters respective unique mannerisms. At the same time, the dialogue should offer a glimpse into ${topic}, showcasing Peterson's intellectual perspective and Rogan's curiosity. The transcript should be concise, with a maximum of 8 back-and-forth exchanges, and structured to be delivered within 45-55 seconds. The person attribute should either be JOE_ROGAN or JORDAN_PETERSON. The line attribute should be a that character's line of dialogue.`,
+				content: `Create a dialogue for a short-form conversation on the topic of ${topic}. The conversation should be between two agents, ${agentA.replace(
+					'_',
+					' '
+				)} and ${agentB}, each with their unique mannerisms and speech patterns. ${agentA.replace(
+					'_',
+					' '
+				)} should engage with the topic with a sense of curiosity and a desire for practical understanding, while ${agentB.replace(
+					'_',
+					' '
+				)} offers a deep, analytical perspective. The dialogue should be engaging and include light humor, yet still provide meaningful insights into ${topic}. Limit the dialogue to a maximum of 8 back-and-forth exchanges, aiming for a concise transcript that would last between 45-55 seconds. The person attribute should either be ${agentA} or ${agentB}. The line attribute should be a that character's line of dialogue. I also need an asset description under the asset attribute which would be a relevant search query to find an image which should be relevant to the overall topic of the conversation. The asset descriptions shouldn't be vague, but a description of something that you think would be a good image to go along with the conversation. Specificity is key.`,
 			},
 		],
 		functions: [
@@ -28,8 +37,9 @@ export default async function transcriptFunction(topic) {
 								properties: {
 									person: { type: 'string' },
 									line: { type: 'string' },
+									asset: { type: 'string' },
 								},
-								required: ['person', 'line'],
+								required: ['person', 'line', 'asset'],
 							},
 						},
 					},

505db952003e504f7bd1e373ff0746dcf5ddf884
Adam Sims <adamsims1203@gmail.com>
2024-02-09 21:18:24 -0500

    transcribe

--- transcribe.mjs
+++ transcribe.mjs
@@ -2,14 +2,40 @@ import OpenAI from 'openai';
 import fs from 'fs';
 import dotenv from 'dotenv';
 import { generateTranscriptAudio } from './eleven.mjs';
+import getAudioDuration from './audioduration.mjs';
 dotenv.config();
+import { readFile, writeFile } from 'fs/promises';
+import concatenateAudioFiles from './concat.mjs';
 
 const openai = new OpenAI({
 	apiKey: process.env.OPENAI_API_KEY,
 });
 
+function srtTimeToSeconds(srtTime) {
+	const [hours, minutes, secondsAndMillis] = srtTime.split(':');
+	const [seconds, milliseconds] = secondsAndMillis.split(',');
+	return (
+		Number(hours) * 3600 +
+		Number(minutes) * 60 +
+		Number(seconds) +
+		Number(milliseconds) / 1000
+	);
+}
+
+function secondsToSrtTime(seconds) {
+	const pad = (num, size) => String(num).padStart(size, '0');
+	const hrs = Math.floor(seconds / 3600);
+	const mins = Math.floor((seconds % 3600) / 60);
+	const secs = Math.floor(seconds % 60);
+	const millis = Math.round((seconds % 1) * 1000); // Round the milliseconds
+	return `${pad(hrs, 2)}:${pad(mins, 2)}:${pad(secs, 2)},${pad(millis, 3)}`;
+}
+
 export default async function transcribeFunction() {
 	const audios = await generateTranscriptAudio();
+	let startingTime = 0;
+
+	concatenateAudioFiles();
 
 	for (const audio of audios) {
 		const transcription = await openai.audio.transcriptions.create({
@@ -19,7 +45,33 @@ export default async function transcribeFunction() {
 			prompt: 'transcribe every single individual word',
 		});
 
-		fs.writeFileSync(`public/srt/${audio.audio}.srt`, String(transcription));
+		const srtContent = String(transcription);
+		const lines = srtContent.split('\n');
+		const incrementedSrtLines = lines.map((line) => {
+			const timeMatch = line.match(
+				/(\d{2}:\d{2}:\d{2},\d{3}) --> (\d{2}:\d{2}:\d{2},\d{3})/
+			);
+			if (timeMatch) {
+				const startTime = srtTimeToSeconds(timeMatch[1]) + startingTime;
+				const endTime = srtTimeToSeconds(timeMatch[2]) + startingTime;
+				const incrementedStartTime = secondsToSrtTime(startTime);
+				const incrementedEndTime = secondsToSrtTime(endTime);
+				return `${incrementedStartTime} --> ${incrementedEndTime}`;
+			}
+			return line;
+		});
+
+		const incrementedSrtContent = incrementedSrtLines.join('\n');
+
+		await writeFile(
+			`public/srt/${audio.person}-${audio.index}.srt`,
+			incrementedSrtContent,
+			'utf8'
+		);
+
+		const duration = await getAudioDuration(audio.audio);
+		// 0.28 is the silence duration
+		startingTime += duration + 0.12;
 	}
 }
 

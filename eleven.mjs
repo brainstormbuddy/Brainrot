687da685f97a96229d86a6dde6187f2a1cc9f318
Adam Sims <adamsims1203@gmail.com>
2024-02-10 12:51:57 -0500

    image fetch and 4 agents

--- eleven.mjs
+++ eleven.mjs
@@ -2,11 +2,13 @@ import fetch from 'node-fetch';
 import fs from 'fs';
 import dotenv from 'dotenv';
 import transcriptFunction from './transcript.mjs';
+import { writeFile } from 'fs/promises';
+import path from 'path';
+
 dotenv.config();
 
-export async function generateTranscriptAudio() {
-	const transcript = (await transcriptFunction('Matrix Multiplication'))
-		.transcript;
+export async function generateTranscriptAudio(topic, agentA, agentB) {
+	let transcript = (await transcriptFunction(topic, agentA, agentB)).transcript;
 
 	const audios = [];
 
@@ -17,15 +19,59 @@ export async function generateTranscriptAudio() {
 		const voice_id =
 			person === 'JOE_ROGAN'
 				? process.env.JOE_ROGAN_VOICE_ID
+				: person === 'BARACK_OBAMA'
+				? process.env.BARACK_OBAMA_VOICE_ID
+				: person === 'BEN_SHAPIRO'
+				? process.env.BEN_SHAPIRO_VOICE_ID
 				: process.env.JORDAN_PETERSON_VOICE_ID;
+
+		const imageFetch = await fetch(
+			`https://www.googleapis.com/customsearch/v1?q=${encodeURI(
+				transcript[i].asset
+			)}&cx=${process.env.GOOGLE_CX}&searchType=image&key=${
+				process.env.GOOGLE_API_KEY
+			}&num=1`,
+			{
+				method: 'GET',
+				headers: { 'Content-Type': 'application/json' },
+			}
+		);
+
+		const imageResponse = await imageFetch.json();
+
+		if (!imageResponse.items || imageResponse.items.length === 0) {
+			throw new Error('Image not found');
+		}
 
 		await generateAudio(voice_id, person, line, i);
 		audios.push({
 			person: person,
 			audio: `public/voice/${person}-${i}.mp3`,
 			index: i,
+			image: imageResponse.items[0].link,
 		});
 	}
+
+	const initialAgentName = audios[0].person;
+
+	const contextContent = `
+  import { staticFile } from 'remotion';
+
+  export const initialAgentName = '${initialAgentName}';
+  export const subtitlesFileName = [
+    ${audios
+			.map(
+				(entry, i) => `{
+      name: '${entry.person}',
+      file: staticFile('srt/${entry.person}-${i}.srt'),
+      asset: '${entry.image}',
+    }`
+			)
+			.join(',\n    ')}
+  ];
+`;
+
+	await writeFile('src/tmp/context.tsx', contextContent, 'utf-8');
 
 	return audios;
 }
@@ -66,5 +112,3 @@ export async function generateAudio(voice_id, person, line, index) {
 		audioStream.on('error', reject);
 	});
 }
-
-await generateTranscriptAudio();
